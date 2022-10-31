4f6d7ddece9db80b237f152c12294a81df0500df
zhuaiballl <ghc@mail.bnu.edu.cn>
2022-10-31 10:17:16 +0800

    create scripts/filedag-run.sh

---
+++ scripts/filedag-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+./lotus fetch-params 2048
+./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
+./lotus-seed genesis new localnet.json
+./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
+tmux new-session -s "lotus-daemon" -d "./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false"
+./lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01000.key
+./lotus-miner init --genesis-miner --actor=t01000 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01000.json --nosync
+tmux new-session -s "lotus-miner" -d "./lotus-miner run --nosync"
